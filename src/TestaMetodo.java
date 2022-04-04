a54769ec3a0154c43172e46a3c71079aef7febd6
João Pedro <jp663687@gmail.com>
2022-04-04 14:38:45 -0300

    05

--- src/TestaMetodo.java
+++ src/TestaMetodo.java
@@ -7,19 +7,19 @@ public class TestaMetodo {
         contajoao.titular.nome = "João Pedro Nogueira";
         contajoao.titular.cpf = "123.434.545-20";
         contajoao.titular.profissao = "Developer";
-        contajoao.saldo = 100;
+        contajoao.deposita(100);
 
         Conta pedro = new Conta();
         pedro.titular = new Cliente();
         pedro.titular.nome = "Pedro";
         pedro.titular.cpf = "987.434.445-20";
         pedro.titular.profissao = "Developer";
-        pedro.saldo = 5000;
+        pedro.deposita(5000);
 
-        System.out.println("Saldo pedro: " + pedro.saldo);
-        System.out.println("Saldo joao: " + contajoao.saldo);
+        System.out.println("Saldo pedro: " + pedro.getSaldo());
+        System.out.println("Saldo joao: " + contajoao.getSaldo());
         pedro.transfere(500, contajoao);
-        System.out.println("Saldo pedro: " + pedro.saldo);
-        System.out.println("Saldo joao: " + contajoao.saldo);
+        System.out.println("Saldo pedro: " + pedro.getSaldo());
+        System.out.println("Saldo joao: " + contajoao.getSaldo());
     }
 }
