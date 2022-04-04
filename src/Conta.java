e4ed677d6f75424b2ab4a96161760850aeb736b1
João Pedro <jp663687@gmail.com>
2022-04-04 10:24:36 -0300

    03

--- src/Conta.java
+++ src/Conta.java
@@ -11,12 +11,23 @@ public class Conta {
     }
 
     // com retorno
-   public boolean saca(double valor){
-       if(this.saldo >= valor){
-           this.saldo = this.saldo - valor;
-           return true;
-       } else{
-           return false;
-       }
-   }
+    public boolean saca(double valor) {
+        if (this.saldo >= valor) {
+            this.saldo = this.saldo - valor;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    // com retorno
+    public boolean transfere(double valor, Conta destino) {
+        if (this.saldo >= valor) {
+            this.saldo = this.saldo - valor;
+            destino.saldo = destino.saldo + valor;
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
--- src/TestaMetodo.java
+++ src/TestaMetodo.java
@@ -2,11 +2,23 @@ public class TestaMetodo {
 
     public static void main(String[] args) {
         Conta joao = new Conta();
+
+
         joao.saldo = 100;
-        System.err.println(joao.saldo);
-        joao.deposita(50);
-        System.out.println(joao.saldo);
-        joao.saca(60);
-        System.out.println(joao.saldo);
+        /*
+         * System.err.println(joao.saldo);
+         * joao.deposita(50);
+         * System.out.println(joao.saldo);
+         * joao.saca(60);
+         * System.out.println(joao.saldo);
+         */
+
+        Conta pedro = new Conta();
+        pedro.saldo = 5000;
+        System.out.println("Saldo pedro: " + pedro.saldo);
+        System.out.println("Saldo joao: " + joao.saldo);
+        pedro.transfere(500, joao);
+        System.out.println("Saldo pedro: " + pedro.saldo);
+        System.out.println("Saldo joao: " + joao.saldo);
     }
-} 
+}
